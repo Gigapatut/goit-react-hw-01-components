5d6d694683ec34af1c5366d9f2d3d2b232326bbe
Gigapatut <125769111+Gigapatut@users.noreply.github.com>
2023-09-08 21:42:41 +0300

    hw-01 first attempt

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import user from 'user';
+import data from 'data';
+import friends from 'friends';
+import transactions from 'transactions';
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/friends/friends';
+import { TransactionHistory } from 'components/transactions/TransactionHistory';
+export const App = () => {
+  return (
+    <div
+      style={{
+        paddingTop: '40px',
+        paddingLeft: '10px',
+        paddingRight: '10px',
+        paddingBottom: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+        backgroundColor: 'rgb(182, 203, 211)',
+      }}
+    >
+      <Profile
+        url={user.avatar}
+        userName={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+};
---
+++ src/components/transactions/TransactionHistory.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css'
+
+export const TransactionHistory = ({ items }) => { 
+  return (
+    <table className={css.transaction}>
+      <thead className={css.thead}>
+        <tr>
+          <th className={css.tableHeader}>Type</th>
+          <th className={css.tableHeader}>Amount</th>
+          <th className={css.tableHeader}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(item => (
+          <tr className={css.tableRow} key={item.id}>
+            <td className={css.tableData}>{item.type}</td>
+            <td className={css.tableData}>{item.amount}</td>
+            <td className={css.tableData}>{item.currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
