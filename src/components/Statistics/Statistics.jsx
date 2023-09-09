825af6ab87e97deaee8e682b29a47cef7286f858
Gigapatut <125769111+Gigapatut@users.noreply.github.com>
2023-09-09 07:01:17 +0300

    hw-01 second attempt

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,10 +2,11 @@ import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
 export const Statistics = ({ title, stats }) => {
-
   return (
     <section className={css.statistics}>
-      {title != null && <h2 className={css.title}>{title}</h2>}
+      {title && 
+        <h2 className={css.title}>{title}</h2>
+      }
       <ul className={css.list}>
         {stats.map(stat => (
           <li className={css.item} key={stat.id}>
